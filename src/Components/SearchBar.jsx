c36b63504948d83f6dcddcb088be4be29ef8a768
shrenik <sutarshrenik3@gmail.com>
2023-07-13 17:30:44 +0530

    Search realted login is done in this branch

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,13 +1,35 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import {FaSearch} from "react-icons/fa"
 import './Searchbar.css'
 
-const SearchBar = () => {
+const SearchBar = ({setResult}) => {
+const [input,setInput]= useState("")
+
+const fetchData=(value)=>{
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((response)=>response.json())
+    .then((json)=>{
+    const Results=json.filter((user=>{
+        return value && user && user.name && user.name.toLowerCase().includes(value)
+    }));
+    setResult(Results)
+    })
+}
+
+
+const hadleChange =(value)=>{
+    setInput(value)
+    fetchData(value)
+}
+
   return (
     <div className='input-wrapper'>
       <FaSearch id="search-icon"></FaSearch>
-      <input placeholder='Type here to search'></input>
+      <input placeholder='Type to search...'
+        value={input}
+        onChange={(e)=>hadleChange(e.target.value)}
+      />
     </div>
   )
 }
